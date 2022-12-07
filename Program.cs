91fcc09b11522317d16a6f4488b4de67a507cbf6
hamzafoy <hamzaoflouisville@gmail.com>
2022-12-07 16:28:49 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+DefaultFilesOptions defaultFileOption = new DefaultFilesOptions();
+defaultFileOption.DefaultFileNames.Clear();
+defaultFileOption.DefaultFileNames.Add("index.html");
+app.UseDefaultFiles(defaultFileOption);
+app.UseStaticFiles();
+
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
