2e23b7612fd428ca4f2a1b686e87b88dcff9cee8
hamzafoy <hamzaoflouisville@gmail.com>
2022-12-08 17:43:12 -0500

    Switched from serving static files to MVC architecture with layout common view and two separate views with AppController.

---
+++ Controllers/AppController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SampleNetCoreMVC.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact Us!";
+            return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using SampleNetCoreMVC.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
+// This is the latest process to give the server the service to have controllers to serve views/webpages.
+
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
-DefaultFilesOptions defaultFileOption = new DefaultFilesOptions();
-defaultFileOption.DefaultFileNames.Clear();
-defaultFileOption.DefaultFileNames.Add("index.html");
-app.UseDefaultFiles(defaultFileOption);
-app.UseStaticFiles();
 
+// Process of serving base .html files without relying on MVC infrastructure
+
+//DefaultFilesOptions defaultFileOption = new DefaultFilesOptions();
+//defaultFileOption.DefaultFileNames.Clear();
+//defaultFileOption.DefaultFileNames.Add("index.html");
+//app.UseDefaultFiles(defaultFileOption);
+
+app.UseStaticFiles();
+app.UseRouting();
+
+
+// This is a more modern and simplified approach to allow the backend to route incoming requests to different
+// endpoints to get the resources needed for the desired response.
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=App}/{action=Index}/{id?}"
+//    );
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute("Default", "/{controller}/{action}/{id?}", new { controller = "App", action = "Index" });
+});
 
 app.Run();
